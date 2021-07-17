6dbb5669699ebd0bd81d4b6e88651343504c1a65
samir <samir.bensalah2012@gmail.com>
2021-07-17 20:35:52 +0100

    change 2 time text

--- SurveillerVotrePoids/ContentView.swift
+++ SurveillerVotrePoids/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, git! bla bla")
+        Text("Hello, git! bla bla 2012")
             .padding()
     }
 }
