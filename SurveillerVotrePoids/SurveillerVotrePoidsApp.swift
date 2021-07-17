aa848b228533643bf78061da0f03424301c6a83c
samir <samir.bensalah2012@gmail.com>
2021-07-17 19:41:37 +0100

    Initial Commit

---
+++ SurveillerVotrePoids/SurveillerVotrePoidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurveillerVotrePoidsApp.swift
+//  SurveillerVotrePoids
+//
+//  Created by MACBOOKAIR on 17/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct SurveillerVotrePoidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
