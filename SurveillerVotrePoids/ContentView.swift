09df736ab249341085f06d73bdbce03d947129f3
samir <samir.bensalah2012@gmail.com>
2021-07-19 19:29:57 +0100

    Add tabview

--- SurveillerVotrePoids/ContentView.swift
+++ SurveillerVotrePoids/ContentView.swift
@@ -9,13 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, git! bla bla 2012")
-            .padding()
+Text("Surveiller votre poids")
+    TabView {
+       accueil()
+         .tabItem {
+            Image(systemName: "house.circle")
+            Text("Accueil")
+       }
+       pesee()
+         .tabItem {
+            Image(systemName: "timer")
+            Text("Pesée")
+      }
+        profil()
+        .tabItem {
+           Image(systemName: "person")
+           Text("Profil")
+     }
+    }
     }
 }
+    
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+struct accueil: View {
+    var body: some View {
+        Color.red
+    }
+}
+struct pesee: View {
+    var body: some View {
+        Color.blue
+    }
+}
+
+struct profil: View {
+    var body: some View {
+        Color.orange
+    }
+}
+
